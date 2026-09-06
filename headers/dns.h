5a053a8def0c1f50556c2d7c09d2aee14312a56f
sigdown <vasilybichutsky@gmail.com>
2026-09-06 14:20:33 +0300

    feat: add simple header and question structures

---
+++ headers/dns.h
@@ -0,0 +1,21 @@
+#ifndef DNS_H
+#define DNS_H
+
+#include <stdint.h>
+
+typedef struct {
+    uint16_t id;
+    uint16_t flags;
+    uint16_t qdcount;
+    uint16_t ancount;
+    uint16_t nscount;
+    uint16_t arcount;
+} dns_header;
+
+typedef struct {
+    uint8_t *qname;
+    uint16_t qtype;
+    uint16_t qclass;
+} question;
+
+#endif
--- main.c
+++ main.c
@@ -1,5 +1,83 @@
+#include <netinet/in.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+typedef struct {
+    uint16_t id;
+    uint16_t flags;
+    uint16_t qdcount;
+    uint16_t ancount;
+    uint16_t nscount;
+    uint16_t arcount;
+} dns_header;
+
+typedef struct {
+    uint8_t *qname;
+    uint16_t qtype;
+    uint16_t qclass;
+} question;
 
 int main(int argc, char **argv) {
-    printf("Hello, dango!\n");
+
+    uint8_t buf[512];
+
+    size_t offset = 0;
+
+    // header
+
+    buf[offset++] = 0x12;
+    buf[offset++] = 0x34;
+
+    buf[offset++] = 0x01;
+    buf[offset++] = 0x00;
+
+    buf[offset++] = 0x00;
+    buf[offset++] = 0x01;
+
+    buf[offset++] = 0x00;
+    buf[offset++] = 0x00;
+
+    buf[offset++] = 0x00;
+    buf[offset++] = 0x00;
+
+    buf[offset++] = 0x00;
+    buf[offset++] = 0x00;
+
+    // question
+
+    buf[offset++] = 7;
+    memcpy(buf + offset, "sigdown", 7);
+    offset += 7;
+
+    buf[offset++] = 3;
+    memcpy(buf + offset, "com", 3);
+    offset += 3;
+
+    buf[offset++] = 0;
+
+    buf[offset++] = 0x00;
+    buf[offset++] = 0x01;
+
+    buf[offset++] = 0x00;
+    buf[offset++] = 0x01;
+
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    struct sockaddr_in server = {
+        .sin_family = AF_INET,
+        .sin_port = htons(53)
+    };
+
+    inet_pton(AF_INET, "8.8.8.8", &server.sin_addr);
+
+    sendto(fd, buf, offset, 0, (struct sockaddr *)&server, sizeof(server));
+
+    unsigned char response[4096];
+
+    ssize_t n = recvfrom(fd, response, sizeof(response), 0, NULL, NULL);
+
+    close(fd);
 }
